80642ba9d9d7daaf06d2e31d49e6631d0ab24735
g0dzzy <zaretsky007@gmail.com>
2023-06-02 11:03:19 +0100

    edit hello.py

--- Hello.py
+++ Hello.py
@@ -3,3 +3,8 @@ print("Welcome to my repo guys")
 print("--------------")
 print("Add new data")
 print("--------------")
+
+
+print("--------------")
+print("Add new data in Visual Studio Code")
+print("--------------")
